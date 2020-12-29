ece8f4108bcec5e8735997e24fdf3f990d24a59a
Gigabyte <gumb1t97@gmail.com>
2020-12-29 21:33:21 +0200

    Added file change come in ProductForm .cs

--- CrmBl/Model/CrmContext.cs
+++ CrmBl/Model/CrmContext.cs
@@ -11,6 +11,7 @@ namespace CrmBl.Model
         public DbSet<Check> Checks { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Product> Products { get; set; }
+        
         public DbSet<Sell> Sells { get; set; }
         public DbSet<Seller> Sellers { get; set; }
     }
--- CrmUI/Catalog.cs
+++ CrmUI/Catalog.cs
@@ -1,22 +1,60 @@
 ﻿using System;
 using System.Data.Entity;
 using System.Windows.Forms;
+using CrmBl.Model;
 
 namespace CrmUI
 {
     public partial class Catalog<T> : Form where T : class
     {
-        public Catalog(DbSet<T> dbSet)
+        private CrmContext _db;
+        private DbSet<T> _set;
+
+        public Catalog(DbSet<T> dbSet, CrmContext db)
         {
             InitializeComponent();
+
+            _db = db;
+            _set = dbSet;
+
+            // Load from Data Base
+            dbSet.Load();
+            // Bound GridView and Data Base
             dataGridView.DataSource = dbSet.Local.ToBindingList();
         }
-        
-        
 
         private void Catalog_Load(object sender, EventArgs e)
         {
-            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Getting firs row is equals id 
+            var id = dataGridView.SelectedRows[0].Cells[0].Value;
+            if (typeof(T) == typeof(Product))
+            {
+                if (_set.Find(id) is Product product)
+                {
+                    // Edit form product at the new form
+                    var form = new ProductForm_Load(product);
+                    if (form.ShowDialog() == DialogResult.OK)
+                    {
+                        product = form.Product;
+                        _db.SaveChanges();
+                        dataGridView.Update();
+                    }
+                }
+            }
+            else if (typeof(T) == typeof(Seller))
+            {
+            }
+            else if (typeof(T) == typeof(Customer))
+            {
+            }
         }
     }
 }
